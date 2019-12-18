3451eaf4b2921b429d7e45fe860db3af66e255b8
Naomi Moisuc <moisucnaomi@silo2480.sciencelogic.local>
2019-12-18 17:48:45 +0200

    interior plic complet

--- src/app/test-animation/test-animation.component.ts
+++ src/app/test-animation/test-animation.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { trigger, transition, animate, style, keyframes, state } from '@angular/animations';
+import { Component, OnInit } from "@angular/core";
+import {
+  trigger,
+  transition,
+  animate,
+  style,
+  keyframes,
+  state
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-test-animation',
-  templateUrl: './test-animation.component.html',
-  styleUrls: ['./test-animation.component.scss']
+  selector: "app-test-animation",
+  templateUrl: "./test-animation.component.html",
+  styleUrls: ["./test-animation.component.scss"]
 })
-
 export class TestAnimationComponent implements OnInit {
   isOpened = false;
   flipDiv = true;
   isTicketOpened = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.isOpened = false;
@@ -23,14 +29,11 @@ export class TestAnimationComponent implements OnInit {
   }
 
   onFlipClick() {
-    if(this.isOpened)
-      this.isOpened = false;
+    if (this.isOpened) this.isOpened = false;
     this.flipDiv = !this.flipDiv;
   }
 
   openTicket() {
     this.isTicketOpened = !this.isTicketOpened;
   }
-
 }
-
